e2a59ccecb025901a56beb26b58295699fe18846
NikkiCoder8055 <114575968+NikkiCoder8055@users.noreply.github.com>
2023-03-12 13:42:30 +0530

    commited

---
+++ src/main/java/com/NodeTester/NodeTester1Application.java
@@ -0,0 +1,17 @@
+package com.NodeTester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 
+ * @author Nikhil
+ *
+ */
+@SpringBootApplication
+public class NodeTester1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodeTester1Application.class, args);
+	}
+
+}
